b991828e19653770c6335d307f95fe6aa5d13c18
petch0403 <petch040304@gmail.com>
2023-07-12 14:04:58 +0700

    init 2

--- ex01.js
+++ ex01.js
@@ -1,4 +1,11 @@
-var t = setInterval(function() {
-    console.log('test2')
-  }, 1000)
-  console.log('test1')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! pichai')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listenin on port ${port}`)
+  console.log('it pichai')})
